1f3d442a678cc7ddd62079941ffc663c2821eeb7
emazou <estefania.mazou@gmail.com>
2022-10-10 22:54:03 -0500

    url modified

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-/* const api_url = 'http://localhost:4000' */
-// let api_url = 'https://mindgrow-back.herokuapp.com'
-let api_url = 'https://mindgrow-back1.herokuapp.com'
+//let api_url = 'http://localhost:4000'
+let api_url = 'https://mindgrow-back.herokuapp.com'
+//let api_url = 'https://mindgrow-back1.herokuapp.com'
 
 export default api_url
